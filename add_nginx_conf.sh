ac7b38a4cc5c2bd7ccd9e55406b6ab98fbf2277d
jrwthll <manolov@dbis.rwth-aachen.de>
2015-11-05 11:16:34 +0100

    check for old config

--- add_nginx_conf.sh
+++ add_nginx_conf.sh
@@ -1,9 +1,9 @@
 #!/bin/bash
 # This script serves to make a LayersBox Adapter aware of a new backend service.
 
-if [$# < 2]
+if [ $# -le 1 ]
  then
-	sn=@2
+	sn=$1
 	echo "The new service is called $sn ."
  else
 	echo "Too many service names specified! Aborting..."
@@ -13,17 +13,26 @@ fi
 td=/usr/local/openresty/conf/services
 echo "The path for all service subdirectories is set to $td ."
 
-if [ -f ./services/documentation/nginx.documentation.conf ]
+out=$(docker ps -a | grep -c adapter-data)
+
+if [ $out -gt 0 ]
  then
-	out=$(docker ps -a | grep -c adapter-data)
-	if [ $out -gt 0 ]
-	 then
-		# create path in Adapter container
-		nsv=$td/documentation
-		docker exec adapter mkdir -p $nsv/
-		# copy nginx configuration entry for service
-		docker cp ./services/documentation/nginx.documentation.conf adapter-data:$nsv
-		# restart nginx daemon in the adapter container
-		docker kill --signal="HUP" adapter
+	echo "Adapter data container found. Checking whether the service has been added already..."
+	nsv=$td/documentation
+	osv=$(docker exec adapter ls $nsv)	
+	
+	if [ -z "$osv"  ]
+	 then 
+		echo "No previous configuration for this service found. Proceeding with setup..."
+	 else
+		echo "Previous configuration detected. Deleting old configuration..."
+		docker exec adapter rm -rf $osv
 	fi
+	
+	docker exec adapter mkdir -p $nsv/
+	echo "Copying service's nginx.conf to newly created path $nsv..."
+	docker cp ./services/documentation/nginx.documentation.conf adapter-data:$nsv
+	echo "Loading newly copied configuration..."
+	docker kill --signal="HUP" adapter
+	echo "Finished."
 fi
