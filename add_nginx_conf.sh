67f36bb5ea075412f37baad03c4203924b86784b
jrwthll <manolov@dbis.rwth-aachen.de>
2015-11-05 10:33:07 +0100

    added parameter skeleton

--- add_nginx_conf.sh
+++ add_nginx_conf.sh
@@ -1,13 +1,19 @@
 #!/bin/bash
 # This script serves to make a LayersBox Adapter aware of a new backend service.
 
+sn=@2
+echo "The new service is called $sn ."
+
+td=/usr/local/openresty/conf/services
+echo "The path for all service subdirectories is set to $td ."
+
 if [ -f ./services/documentation/nginx.documentation.conf ]
  then
 	out=$(docker ps -a | grep -c adapter-data)
 	if [ $out -gt 0 ]
 	 then
 		# create path in Adapter container
-		nsv=/usr/local/openresty/conf/services/documentation
+		nsv=$td/documentation
 		docker exec adapter mkdir -p $nsv/
 		# copy nginx configuration entry for service
 		docker cp ./services/documentation/nginx.documentation.conf adapter-data:$nsv
