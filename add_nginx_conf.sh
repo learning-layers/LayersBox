414bcab568aafc1f6aacac7700111cdacfcc503e
jrwthll <manolov@dbis.rwth-aachen.de>
2015-11-05 10:14:22 +0100

    removed obsolete comment

--- add_nginx_conf.sh
+++ add_nginx_conf.sh
@@ -1,6 +1,5 @@
 #!/bin/bash
 # This script serves to make a LayersBox Adapter aware of a new backend service.
-# Will become obsolete in the future as there should be only one such script included in the default LayersBox.
 
 if [ -f ./services/documentation/nginx.documentation.conf ]
  then
